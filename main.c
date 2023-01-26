d28c82bb049ea238d4c119841e763971d01e3388
Anonm13 <micheljanin296@gmail.com>
2023-01-26 17:52:52 +0100

    fix function

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 void activate(GtkApplication* app, gpointer user_data){
 	GtkWidget *mainWindow;
 	
-	mainWindow = gtk_window_new(app);
+	mainWindow = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(mainWindow), "Hello World");
 	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 200, 200);
 	gtk_widget_show(mainWindow);

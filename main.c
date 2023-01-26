622353b67641c23f4624ba9448342bf161c509de
Anonm13 <micheljanin296@gmail.com>
2023-01-26 16:06:38 +0100

    window-creation

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "includes/header.h"
+#include <gtk/gtk.h>
 
 int main(int argc, char** argv){
-	hello();
+	gtk_init(&argc, &argv);
+
+	main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(main_window), "Hello World");
+	gtk_widget_show(window);
+
+	gtk_main();
+	
 	return EXIT_SUCCESS;
 }

998308d61d7c356f58bdd7d9a6f2d050ab684c9a
Anonm13 <micheljanin296@gmail.com>
2023-01-26 18:20:08 +0100

    fix

--- main.c
+++ main.c
@@ -3,15 +3,6 @@
 #include <gtk/gtk.h>
 #include "header.h"
 
-void activate(GtkApplication* app, gpointer user_data){
-	GtkWidget *mainWindow;
-	
-	mainWindow = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(mainWindow), "Hello World");
-	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 200, 200);
-	gtk_widget_show(mainWindow);
-}
-
 int main(int argc, char** argv){
 	GtkApplication *app;
 	int status;

605f117b0a28dfab5d8a1a513802158efbca6f3f
Anonm13 <micheljanin296@gmail.com>
2023-01-26 16:47:42 +0100

    follow doc

--- main.c
+++ main.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-int main(int argc, char** argv){
-	GtkWidget *main_window;
+void activate(GtkApplication* app, gpointer user_data){
+	GtkWidget *mainWindow;
 
 	gtk_init(&argc, &argv);
 
-	main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(main_window), "Hello World");
-	gtk_widget_show(main_window);
+	mainXindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(mainWindow), "Hello World");
+	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 200, 200);
+	gtk_widget_show(mainWindow);
+}
 
-	gtk_main();
+int main(int argc, char** argv){
+	GtkApplication *app;
+	int status;
 
-	return EXIT_SUCCESS;
+	app = gtk_application_new("org.esgi.hotel", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+	return status;
 }

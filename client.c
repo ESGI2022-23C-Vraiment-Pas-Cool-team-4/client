ea08ceeea0a6a5a842dceb0351947aa47a2383d6
Charbel Salhab <118256379+csalhabb@users.noreply.github.com>
2023-02-10 22:58:57 +0100

    replace the old client.c file by the new one

--- client.c
+++ client.c
@@ -5,87 +5,52 @@
  * The rooms can be reserved by customers according to the criteria of the hotel."
  */
 
+#include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "includes/sockets.h"
 
 int main(){
 
-    char ip[16];
-    int port;
-
-    // opening the conf file to get the ip address of the server and the port number
-    FILE *config_file = fopen("server.conf", "r");
-    
-    if (config_file == NULL) {
-        printf("Error opening config file\n");
-        return 1;
-    }
-
-    fscanf(config_file, "%s %d", ip, &port);
-
-    int sock;
-
-    struct sockaddr_in addr;
-    
-    socklen_t addr_size;
-
-    char buffer[1024];
-    int n;
-
     //create the TCP server socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(sock < 0){
-        perror("[-]Socket error");
-        exit(1);
-    }
+    int socketClient = socket(AF_INET,SOCK_STREAM,0);
 
-    printf("[+]TCP server socket created. \n");
-    
+    struct sockaddr_in addrClient;
+
     /*
-    set up the socket address structure for connecting to a server.
+    1-set the address family to be used as the Internet Protocol v4.
 
-    1-initialize the memory for the address structure to all zeroes.
+    2-set the port number to be used for the connection.
 
-    2-sets the address family to be used as the Internet Protocol v4.
-
-    3-set the port number to be used for the connection.
-
-    4-set the IP address to be used for the connection using the inet_addr function which converts the passed IP address string to an 32-bit integer.
+    3-set the IP address to be used for the connection using the inet_addr function which converts the passed IP address string to an 32-bit integer.
     */
-    
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = port;
-    addr.sin_addr.s_addr = inet_addr(ip);
+
+    addrClient.sin_addr.s_addr = inet_addr("141.94.70.142");
+    addrClient.sin_family = AF_INET;
+    addrClient.sin_port = htons(10000);
 
     //connect to the server
-    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-    printf("[+]Connected to the server.\n");
+    connect(socketClient, (const struct sockaddr *)&addrClient, sizeof(addrClient));
+    // printf("connecté\n");
 
+    User user;
+    char msg[60];
+    recv(socketClient, msg, 60, 0);
 
-    bzero(buffer, 1024);
-    strcpy(buffer, "HELLO, THIS IS CLIENT.");
-    
-    printf("Client : %s\n", buffer);
-    
+    printf("%s\n",msg);
+    scanf("%s %s", user.nom,user.mdp);
+
     //send data over a socket connection
-    send(sock, buffer, strlen(buffer), 0);
-
-    bzero(buffer, 1024);
+    send(socketClient, &user, sizeof(user), 0);
 
     //close the socket, disconnect from the server
-    recv(sock, buffer, sizeof(buffer), 0);
-    printf("Server : %s\n", buffer);
+    close(socketClient);
 
-    //close the socket, disconnect from the server
-    close(sock);
-    printf("[+]Disconnected from the server.\n");
-
-    //closing the conf file
-    fclose(config_file);
     return 0;
 }

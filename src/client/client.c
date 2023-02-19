52603ba2a2deadece18ec7912a92859ce98f3ba2
Charbel Salhab <118256379+csalhabb@users.noreply.github.com>
2023-02-19 22:34:28 +0100

    Update client.c

--- src/client/client.c
+++ src/client/client.c
@@ -9,7 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
-#include "../../includes/sockets.h"
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -31,18 +30,46 @@ int charToInt(char* str) {
     return result;
 }
 
-int main() {
+char* checklogin(){
+    
+    char *nom,*mdp;
+    query *user;
 
+    user->type = "GetUser";
+    user->paramsName = malloc(sizeof(char*)* 20);
+    user->paramsValue = malloc(sizeof(char*)*20);
+    user->paramsName[0] = "username";
+    user->paramsName[1]="password";
+
+    printf("login : ");
+    scanf("%s", nom);
+    printf("mdp : ");
+    scanf("%s", mdp);
+
+    user->paramsValue[0]= nom;
+    user->paramsValue[1] = mdp;
+    char* requete = concatene(user);
+    return requete;
+
+}
+
+
+int client(char *query) {
     WSADATA wsa;
-    SOCKET client_socket;
-    struct sockaddr_in server_addr;
-
+    SOCKET sock;
+    struct sockaddr_in server;
     char ip[16];
     char port[6];
 
-    // opening the conf file to get the ip address of the server and the port number
-    FILE* config_file = fopen("server.conf", "r");
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+        printf("Failed. Error Code : %d", WSAGetLastError());
+        return 1;
+    }
 
+    // opening the conf file to get the ip address of the server and the port number
+    FILE* config_file = fopen("conf/server.conf", "r");
+    
     if (config_file == NULL) {
         printf("Error opening config file\n");
         return 1;
@@ -50,57 +77,80 @@ int main() {
 
     fscanf(config_file, "%s %s", ip, port);
 
+    // Create the TCP server socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (sock == INVALID_SOCKET) {
+        printf("Error creating socket: %d", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
     int p = charToInt(port);
+    
+    // Set up the address for the server
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(ip);
+    server.sin_port = htons(p);
+    //printf("[+]TCP server socket created. \n");
 
-    //initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("WSAStartup failed. Error Code : %d", WSAGetLastError());
-        return 1;
-    }
-
-    //create a socket for the client
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Failed to create socket. Error Code : %d", WSAGetLastError());
+    // Connect to the server
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+        printf("Error connecting to server: %d", WSAGetLastError());
+        closesocket(sock);
         WSACleanup();
         return 1;
     }
 
-    //configure the server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    server_addr.sin_port = htons(p);
+    //printf("[+]Connected to the server.\n");
 
-    //connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Connection error\n");
+    // Create buffers to store the name and mdp fields
+    char buffer[1024];
+    
+    //char username[100];
+    //char password[100];
+    
+    //printf("Client : %s, %s\n", buffer1, buffer2);
+    
+    //sscanf(query, "querry: GetUser\nparams:\nusername: %s\npassword: %s", username, password);
+    
+    // Send the name buffer over a socket connection
+    if (send(sock, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
+        printf("Error sending name data: %d", WSAGetLastError());
+        closesocket(sock);
         WSACleanup();
         return 1;
     }
 
-    User user;
-    char msg[60];
-    recv(client_socket, msg, 60, 0);
+    // // Send the mdp buffer over a socket connection
+    // if (send(sock, password, strlen(password), 0) == SOCKET_ERROR) {
+    //     printf("Error sending mdp data: %d", WSAGetLastError());
+    //     closesocket(sock);
+    //     WSACleanup();
+    //     return 1;
+    // }
+    memset(buffer, 0, sizeof(buffer));
 
-    printf("%s\n", msg);
-    scanf("%s %s", user.nom, user.mdp);
-
-    //send data to the server
-    char message[60];
-    snprintf(message, 60, "%s %s", user.nom, user.mdp);
-
-    if (send(client_socket, message, strlen(message), 0) < 0) {
-        printf("Send failed. Error Code : %d", WSAGetLastError());
-        closesocket(client_socket);
+    // Receive data from the server
+    if (recv(sock, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
+        printf("Error receiving data: %d", WSAGetLastError());
+        closesocket(sock);
         WSACleanup();
         return 1;
     }
+    
+    // printf("Server: %s\n", buffer);
 
-    //close the socket and cleanup Winsock
-    closesocket(client_socket);
-    WSACleanup();
+    // Close the socket, disconnect from the server
+    closesocket(sock);
+    //printf("[+]Disconnected from the server.\n");
 
-    //closing the conf file
+    // Closing the conf file
     fclose(config_file);
+
+    // Clean up Winsock
+    WSACleanup();
 
     return 0;
 }
+

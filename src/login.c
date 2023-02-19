99cb66f3e6ad76a206361c6c322939b0b24c8038
Janin Michel-Mathias <107043504+Janin-Michel-Mathias@users.noreply.github.com>
2023-02-19 23:53:58 +0100

    modif (#13)

--- src/login.c
+++ src/login.c
@@ -3,6 +3,8 @@
 int login(char* username, char* password){
     if(!strcmp(username, "azerty") && !strcmp(password, "azerty")){
         return 1;
+    }else if(!strcmp(username, "admin") && !strcmp(password, "admin")){
+        return 2;
     }
     return 0;
 }
